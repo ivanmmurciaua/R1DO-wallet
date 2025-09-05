98fa6e40975d41c43d8c083bdd56b4edb7a448d5
@ivanmmurciaua <solucionesemecuadrado@gmail.com>
2025-09-05 11:07:39 +0200

    Change SC by env

--- src/app/constants.tsx
+++ src/app/constants.tsx
@@ -3,4 +3,4 @@ export const CHAIN_NAME = "arbitrum-sepolia";
 export const PAYMASTER_ADDRESS = "0x888888888888Ec68A58AB8094Cc1AD20Ba3D2402";
 export const BUNDLER_URL = `https://api.pimlico.io/v1/${CHAIN_NAME}/rpc?add_balance_override&apikey=${process.env.NEXT_PUBLIC_PIMLICO_API_KEY}`;
 export const PAYMASTER_URL = `https://api.pimlico.io/v2/${CHAIN_NAME}/rpc?add_balance_override&apikey=${process.env.NEXT_PUBLIC_PIMLICO_API_KEY}`;
-export const REGISTRY_ADDRESS = "0x361af6EfDc737E4079D381aF8fD93760DaCACF4F";
+export const REGISTRY_ADDRESS = process.env.NEXT_PUBLIC_REGISTRY_ADDRESS;
