4bcb0a6f390496ad63dee3c1c40f0248b85e718f
@ivanmmurciaua <solucionesemecuadrado@gmail.com>
2025-09-05 11:13:57 +0200

    Type SC address constant

--- src/app/constants.tsx
+++ src/app/constants.tsx
@@ -1,6 +1,9 @@
+import { Address } from "viem";
+
 export const RPC_URL = "https://sepolia-rollup.arbitrum.io/rpc";
 export const CHAIN_NAME = "arbitrum-sepolia";
 export const PAYMASTER_ADDRESS = "0x888888888888Ec68A58AB8094Cc1AD20Ba3D2402";
 export const BUNDLER_URL = `https://api.pimlico.io/v1/${CHAIN_NAME}/rpc?add_balance_override&apikey=${process.env.NEXT_PUBLIC_PIMLICO_API_KEY}`;
 export const PAYMASTER_URL = `https://api.pimlico.io/v2/${CHAIN_NAME}/rpc?add_balance_override&apikey=${process.env.NEXT_PUBLIC_PIMLICO_API_KEY}`;
-export const REGISTRY_ADDRESS = process.env.NEXT_PUBLIC_REGISTRY_ADDRESS;
+export const REGISTRY_ADDRESS = process.env
+  .NEXT_PUBLIC_REGISTRY_ADDRESS as Address;
